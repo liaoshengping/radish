8763f5ca7d48dccf3839b1c7644e8a39afec417f
廖 <lsp@nongline.com>
2019-10-11 17:00:08 +0800

    测试composer时间

--- src/Commands/MakeAPICommand.php
+++ src/Commands/MakeAPICommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Input\InputOption;
 class MakeAPICommand extends GeneratorCommand
 {
     /**
+     * 2019年10月11日 16:59:33
      * The console command name.
      *
      * @var string
